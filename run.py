f73d2973b874651a8a75148e27efd468fa886a26
Michael Kornilich <molego16@gmail.com>
2025-05-14 22:10:11 +0200

    Brushed up the user interaction part (the app)

--- run.py
+++ run.py
@@ -22,13 +22,23 @@ class Shell(cmd.Cmd):
 
     Available functions:
         - spell <number>: spells number(s)
+        - separator <new separator or nothing>: 
+            Default = "."
+            define a separator which is used to split the whole parts of the numbers.
+            The separator is not strictly enforced so calling 1......4 will return fourteen.
+        - decimal <new decimal separator or nothing>: 
+            Default = ","
+            define a decimal separator. 
+            The decimal separator is used to split the integer from its decimal part. 
+            It IS strictly enforced and the spell function will not work
+            if the decimal separator is found in the target number more than once.
         - exit: exits the script
     """
 
     def __init__(self):
         super().__init__()
-        self.separator = ","
-        self.decimal = "."
+        self.separator = "."
+        self.decimal = ","
 
     def do_separator(self, sep):
         if len(sep) == 0:
@@ -42,7 +52,7 @@ class Shell(cmd.Cmd):
 
     def do_decimal(self, dec):
         if len(dec) == 0:
-            print(f"Current separator: {self.separator}")
+            print(f"Current separator: {self.decimal}")
         elif len(dec) == 1:
             self.decimal = dec
             print(f"Changed separator to: {dec}")
@@ -55,7 +65,7 @@ class Shell(cmd.Cmd):
         spell <number>
         Spells the numbers passed to it.
         Floating point numbers will be truncated.
-        To simplify the input, the usage of "_", " ", "," is permitted to separate the number.
+        To simplify the input, the usage of "_" and the defined separator is permitted to separate the number.
         """
         try:
             num_ = _parse_num(num, separator=self.separator, decimal=self.decimal)
